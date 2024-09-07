218599b271715c65a8485635222db7800046f25c
Marius Smytzek <marius.smytzek@cispa.de>
2024-09-07 14:31:41 +0200

    fitness provides the causes of failing constraints

--- src/fandango/language/convert.py
+++ src/fandango/language/convert.py
@@ -1,5 +1,8 @@
 from typing import List
 
+from cssselect2.parser import AttributeSelector
+
+from fandango.constraints.base import ConjunctionConstraint, DisjunctionConstraint
 from fandango.language.grammar import (
     Grammar,
     Alternative,
@@ -11,6 +14,7 @@ from fandango.language.grammar import (
 )
 from fandango.language.parser.FandangoParser import FandangoParser
 from fandango.language.parser.FandangoParserVisitor import FandangoParserVisitor
+from fandango.language.search import AttributeSearch, RuleSearch
 
 
 class FandangoSplitter(FandangoParserVisitor):
@@ -27,12 +31,6 @@ class FandangoSplitter(FandangoParserVisitor):
 
     def visitPython(self, ctx: FandangoParser.PythonContext):
         self.visitChildren(ctx.python_tag())
-
-    def visitSimple_stmts(self, ctx: FandangoParser.Simple_stmtsContext):
-        self.python_code.append(ctx)
-
-    def visitCompound_stmt(self, ctx: FandangoParser.Compound_stmtContext):
-        self.python_code.append(ctx)
 
 
 class GrammarProcessor(FandangoParserVisitor):
@@ -64,3 +62,72 @@ class GrammarProcessor(FandangoParserVisitor):
             return self.visit(ctx.alternative())
         else:
             raise ValueError(f"Unknown symbol: {ctx.getText()}")
+
+
+class ConstraintProcessor(FandangoParserVisitor):
+    def __init__(self):
+        self.searches = SearchProcessor()
+
+    def get_constraints(self, constraints: List[FandangoParser.ConstraintContext]):
+        return ConjunctionConstraint(
+            [self.visit(constraint) for constraint in constraints]
+        )
+
+    def visitConstraint(self, ctx: FandangoParser.ConstraintContext):
+        return self.visit(ctx.implies())
+
+    def visitImplies(self, ctx: FandangoParser.ImpliesContext):
+        return self.visit(ctx.quantifier())
+
+    def visitQuantifier(self, ctx: FandangoParser.QuantifierContext):
+        if ctx.disjunction():
+            return self.visit(ctx.disjunction())
+
+    def visitDisjunction(self, ctx: FandangoParser.DisjunctionContext):
+        return DisjunctionConstraint(
+            [self.visit(constraint) for constraint in ctx.conjunction()]
+        )
+
+    def visitConjunction(self, ctx: FandangoParser.ConjunctionContext):
+        return ConjunctionConstraint(
+            [self.visit(constraint) for constraint in ctx.formula_atom()]
+        )
+
+    def visitFormula_atom(self, ctx: FandangoParser.Formula_atomContext):
+        if ctx.implies():
+            return self.visit(ctx.implies())
+        elif ctx.expression:
+            return self.visit(ctx.expression())
+        elif ctx.formula_comparison():
+            return self.visit(ctx.formula_comparison())
+        else:
+            raise ValueError(f"Unknown formula atom: {ctx.getText()}")
+
+    def visitExpression(self, ctx: FandangoParser.ExpressionContext):
+        if ctx.selector():
+            expr, searches = self.searches.visit(ctx.selector())
+        elif ctx.expr():
+            expr, searches = self.searches.visit(ctx.expr())
+        else:
+            raise ValueError(f"Unknown expression: {ctx.getText()}")
+
+
+class SearchProcessor(FandangoParserVisitor):
+    def defaultResult(self):
+        return []
+
+    def aggregateResult(self, aggregate, nextResult):
+        return aggregate + nextResult
+
+    def visitSelector(self, ctx: FandangoParser.SelectorContext):
+        if ctx.selector():
+            return [
+                AttributeSearch(
+                    self.visit(ctx.selector()), self.visit(ctx.selection()), None
+                )
+            ]
+        else:
+            return self.visit(ctx.selection())
+
+    def visitSelection(self, ctx: FandangoParser.SelectionContext):
+        return [RuleSearch(NonTerminal(ctx.RULE_NAME().getText()), None)]
