56290fdc4aa516276301839cb9a0befc0fe08134
joszamama <joszamama@gmail.com>
2024-10-28 14:52:26 +0100

    feat: xml eval

--- src/evaluation/evaluation/run_evaluation.py
+++ src/evaluation/evaluation/run_evaluation.py
@@ -1,4 +1,4 @@
-from typing import List
+from typing import List, Tuple
 
 from sklearn.feature_extraction.text import CountVectorizer
 from sklearn.metrics import jaccard_score
@@ -9,13 +9,24 @@ from evaluation.evaluation.xml_evaluation.xml_evaluation import evaluate_xml
 from fandango.language.tree import DerivationTree
 
 
-
+# Return the evaluation results as a tuple of values (subject, total, valid, percentage, diversity, mean_length, median)
+def better_print_results(results: Tuple[str, int, int, float, float, float, float]):
+    print("================================")
+    print(f"{results[0]} Evaluation Results")
+    print("================================")
+    print(f"Total inputs: {results[1]}")
+    print(f"Valid {results[0]} solutions: {results[2]} ({results[3]:.2f}%)")
+    print(f"Diversity: {results[4]:.2f}")
+    print(f"Mean length: {results[5]:.2f}")
+    print(f"Median length: {results[6]:.2f}")
+    print("")
+    print("")
 
 
 def run_evaluation():
-    evaluate_csv()
-    evaluate_rest()
-    evaluate_xml()
+    # better_print_results(evaluate_csv())
+    # better_print_results(evaluate_rest())
+    better_print_results(evaluate_xml())
 
 
 if __name__ == "__main__":
