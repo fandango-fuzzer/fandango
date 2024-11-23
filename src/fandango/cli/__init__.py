6f672784382129856db3cf466a6dc8a85e5aa693
Andreas Zeller <zeller@cispa.de>
2024-11-23 15:46:56 +0100

    Prepared for `fuzz` command

--- src/fandango/cli/__init__.py
+++ src/fandango/cli/__init__.py
@@ -3,76 +3,161 @@ import importlib.metadata
 import logging
 import os
 import sys
+import textwrap
 
 from fandango.cli.interactive import Interactive
-from fandango.constants import INTERACTIVE
+from fandango.constants import INTERACTIVE, FUZZ, HELP
 from fandango.logger import LOGGER
 
 
 def get_parser():
     arguments = argparse.ArgumentParser(
-        description="The access point to the Fandango framework"
+        prog="fandango",
+        description="The access point to the Fandango framework",
+        formatter_class=argparse.RawDescriptionHelpFormatter,
+        epilog=textwrap.dedent("""\
+                               Use `%(prog)s help` to get a list of commands.
+                               Use `%(prog)s help COMMAND` to learn more about COMMAND.""")
     )
 
     arguments.add_argument(
         "--version",
         action="version",
-        version=importlib.metadata.version("fandango"),
-        help="Show version number",
+        version="%(prog)s " + importlib.metadata.version("fandango"),
+        help="show version number",
     )
 
     arguments.add_argument(
-        "-v",
-        "--verbose",
+        "-v", "--verbose",
         dest="verbose",
+        action="store_true",
         help="enable verbose output",
     )
 
     # The subparsers
     commands = arguments.add_subparsers(
-        help="The command of the fandango framework to execute",
+        title="commands",
+        # description="Valid commands",
+        help="the command to execute",
         dest="command",
         required=True,
     )
 
-    interactive_parser = commands.add_parser(
-        INTERACTIVE, help="Open the interactive command line interface"
+    # Commands
+
+    # Fuzz
+    fuzz_parser = commands.add_parser(
+        FUZZ,
+        help="produce outputs from .fan files"
+    )
+    fuzz_parser.add_argument(
+        "-n", "--num-outputs",
+        type=int,
+        dest="num_outputs",
+        default=1,
+        help="the number of outputs to produce (default: 1)",
+    )
+    fuzz_parser.add_argument(
+        "-o", "--output",
+        type=int,
+        dest="output",
+        default=None,
+        help="write output to OUTPUT (default: stdout)",
+    )
+    fuzz_parser.add_argument(
+        "-c", "--constraint",
+        type=int,
+        dest="constraint",
+        default=None,
+        action="append",
+        help="define an additional constraint CONSTRAINT",
+    )
+    fuzz_parser.add_argument(
+        "-d", "--directory",
+        type=int,
+        dest="directory",
+        default=None,
+        help="create individual output files in DIRECTORY",
+    )
+    fuzz_parser.add_argument(
+        "fan-file",
+        type=argparse.FileType('r'),
+        nargs="+",
+        default=None,
+        help="Fandango file (.fan, .py) to be processed",
     )
 
+
     # Interactive
+    interactive_parser = commands.add_parser(
+        INTERACTIVE,
+        help="open the interactive command line interface"
+    )
     interactive_parser.add_argument(
-        "-f",
-        "--fan",
+        "-f", "--fan",
         type=str,
         dest="fan",
         default=None,
-        help="The fan file to use for the interactive mode",
+        help="the fan file to use for the interactive mode",
     )
     interactive_parser.add_argument(
-        "-g" "--grammar",
+        "-g", "--grammar",
         type=str,
         dest="grammar",
         default=None,
-        help="The grammar to use for the interactive mode",
+        help="the grammar to use for the interactive mode",
     )
     interactive_parser.add_argument(
-        "-c",
-        "--constraints",
+        "-c", "--constraints",
         type=str,
         dest="constraints",
         default=None,
-        help="The constraints to use for the interactive mode",
+        help="the constraints to use for the interactive mode",
     )
     interactive_parser.add_argument(
-        "-p",
-        "--python",
+        "-p", "--python",
         type=str,
         dest="python",
         default=None,
-        help="The python code to use in the interactive mode",
+        help="the Python code to use in the interactive mode",
     )
+
+
+    # Help
+    help_parser = commands.add_parser(
+        HELP,
+        help="show this help and exit",
+    )
+    help_parser.add_argument(
+        "help_command",
+        type=str,
+        nargs="*",
+        default=None,
+        help="command to get help on",
+    )
+
     return arguments
 
+def interactive(args):
+    try:
+        interactive_ = Interactive(
+            args.fan, args.grammar, args.constraints, args.python
+        )
+    except Exception as e:
+        print(f"Error during initialization: {type(e)}")
+        sys.exit(1)
+
+    try:
+        interactive_.run()
+    except Exception as e:
+        print(f"Error during execution: {type(e)}")
+        sys.exit(1)
+
+def help(parser, args):
+    for cmd in args.help_command:
+        parser.parse_args([cmd] + ["--help"])
+    else:
+        parser.print_help()
 
 def main(*args: str, stdout=sys.stdout, stderr=sys.stderr):
     if "-O" in sys.argv:
@@ -84,25 +169,18 @@ def main(*args: str, stdout=sys.stdout, stderr=sys.stderr):
     if stderr is not None:
         sys.stderr = stderr
 
-    args = get_parser().parse_args(args or sys.argv[1:])
+    parser = get_parser()
+    args = parser.parse_args(args or sys.argv[1:])
 
     if args.verbose:
         LOGGER.setLevel(logging.DEBUG)
 
     if args.command == INTERACTIVE:
-        try:
-            interactive_ = Interactive(
-                args.fan, args.grammar, args.constraints, args.python
-            )
-        except Exception as e:
-            print(f"Error during initialization: {type(e)}")
-            sys.exit(1)
-
-        try:
-            interactive_.run()
-        except Exception as e:
-            print(f"Error during execution: {type(e)}")
-            sys.exit(1)
+        interactive(args)
+    elif args.command == FUZZ:
+        fuzz(args)
+    elif args.command == HELP:
+        help(parser, args)
 
 if __name__ == "__main__":
     if "-O" in sys.argv:
