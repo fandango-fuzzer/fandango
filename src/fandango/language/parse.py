80574f60c45c52a62e71c082c8f2237e37e6f91e
Andreas Zeller <zeller@cispa.de>
2024-12-14 16:29:03 +0100

    New: caching parser

--- src/fandango/language/parse.py
+++ src/fandango/language/parse.py
@@ -1,10 +1,15 @@
 import ast
 import re
+import os
 import sys
-from typing import Tuple, List
+
+from typing import Tuple, List, Any
 from fandango.logger import LOGGER, print_exception
 
 from antlr4 import InputStream, CommonTokenStream
+import hashlib
+import pickle
+import cachedir_tag
 
 from fandango.constraints import predicates
 from fandango.constraints.base import Constraint
@@ -145,48 +150,33 @@ def check_constraints_existence_children(
     indirect_child[f"<{parent}>"][f"<{symbol}>"] = True
     return True
 
+GLOBALS = predicates.__dict__
+LOCALS = None  # Must be None to ensure top-level imports
 
-def parse(fan_contents: str, /, lazy: bool = False,
-          check_constraints: bool = True, given_grammar=None) -> Tuple[Grammar, List[Constraint]]:
-    """
-    Extract grammar and constraints from the given content
-    :param fan: Fandango specification
-    :param lazy: If True, the constraints are evaluated lazily
-    :param check_constraints: If True, check if the constraints contain non-terminal symbols that are not in the grammar
-    """
+def run_code(code_tree, global_vars, local_vars):
+    code_text = ast.unparse(code_tree)
+    exec(code_text, global_vars, local_vars)
 
-    LOGGER.debug("Setting up .fan parser")
-    input_stream = InputStream(fan_contents)
-    error_listener = MyErrorListener()
-    lexer = FandangoLexer(input_stream)
-    lexer.addErrorListener(error_listener)
-    token_stream = CommonTokenStream(lexer)
-    parser = FandangoParser(token_stream)
-    parser.addErrorListener(error_listener)
-
-    LOGGER.debug("Parsing .fan content")
-    tree = parser.fandango()
-
-    LOGGER.debug("Extracting code")
+def split_tree(tree: Any, lazy: bool = False):
     splitter = FandangoSplitter()
     splitter.visit(tree)
-    global_vars = predicates.__dict__.copy()
-    local_vars = None  # Must be None to ensure top-level imports
+    global_vars = GLOBALS.copy()
+    local_vars = LOCALS
     python_processor = PythonProcessor()
+
+    LOGGER.debug("Extracting code")
     code_tree = python_processor.get_code(splitter.python_code)
-    code_text = ast.unparse(code_tree)
 
     LOGGER.debug("Running code")
+    code_text = ast.unparse(code_tree)
     exec(code_text, global_vars, local_vars)
 
     LOGGER.debug("Extracting grammar")
     grammar_processor = GrammarProcessor(
-        local_variables=local_vars, global_variables=global_vars
+        local_variables=local_vars,
+        global_variables=global_vars
     )
     grammar: Grammar = grammar_processor.get_grammar(splitter.productions)
-
-    if len(grammar.rules) > 0:
-        check_grammar(grammar)
 
     LOGGER.debug("Extracting constraints")
     constraint_processor = ConstraintProcessor(
@@ -198,6 +188,67 @@ def parse(fan_contents: str, /, lazy: bool = False,
     constraints: List[Constraint] = \
         constraint_processor.get_constraints(splitter.constraints)
 
+    return code_tree, grammar, constraints, global_vars, local_vars
+
+
+CACHE_DIR = ".fandango_cache"
+
+def parse(fan_contents: str, /, lazy: bool = False,
+          check_constraints: bool = True, given_grammar=None, use_cache: bool = True) -> Tuple[Grammar, List[Constraint]]:
+    """
+    Extract grammar and constraints from the given content
+    :param fan: Fandango specification
+    :param lazy: If True, the constraints are evaluated lazily
+    :param check_constraints: If True, check if the constraints contain non-terminal symbols that are not in the grammar
+    """
+    from_cache = False
+
+    if use_cache:
+        if not os.path.exists(CACHE_DIR):
+            os.makedirs(CACHE_DIR)
+            cachedir_tag.tag(CACHE_DIR)
+
+        hash = hashlib.sha256(fan_contents.encode()).hexdigest()
+        pickle_file = os.path.join(CACHE_DIR, hash + '.pickle')
+
+        if os.path.exists(pickle_file):
+            try:
+                with open(pickle_file, 'rb') as fp:
+                    LOGGER.info(f"Loading cached spec from {pickle_file}")
+                    spec = pickle.load(fp)
+                from_cache = True
+            except Exception as e:
+                print_exception(e)
+
+    try:
+        code_tree, grammar, constraints, global_vars, local_vars = spec
+    except Exception:
+        from_cache = False
+
+    if not from_cache:
+        LOGGER.debug("Setting up .fan parser")
+        input_stream = InputStream(fan_contents)
+        error_listener = MyErrorListener()
+        lexer = FandangoLexer(input_stream)
+        lexer.addErrorListener(error_listener)
+        token_stream = CommonTokenStream(lexer)
+        parser = FandangoParser(token_stream)
+        parser.addErrorListener(error_listener)
+
+        LOGGER.debug("Parsing .fan content")
+        tree = parser.fandango()
+
+        LOGGER.debug("Splitting content")
+        spec = split_tree(tree, lazy)
+        code_tree, grammar, constraints, global_vars, local_vars = spec
+
+    if from_cache:
+        LOGGER.debug("Running code")
+        run_code(code_tree, global_vars, local_vars)
+
+    if len(grammar.rules) > 0:
+        check_grammar(grammar)
+
     if check_constraints:
         if not grammar or len(grammar.rules) == 0:
             g = given_grammar
@@ -205,6 +256,14 @@ def parse(fan_contents: str, /, lazy: bool = False,
             g = grammar
         if g and len(g.rules) > 0:
             check_constraints_existence(g, constraints)
+
+    if use_cache and not from_cache:
+        try:
+            with open(pickle_file, 'wb') as fp:
+                LOGGER.info(f"Saving spec to cache {pickle_file}")
+                pickle.dump(spec, fp)
+        except Exception as e:
+            print_exception(e)
 
     LOGGER.debug("Parsing complete")
     return grammar, constraints
