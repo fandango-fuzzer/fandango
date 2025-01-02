0d47e07f8c5304742b8b9127e48ad3b66b64ba04
Andreas Zeller <zeller@cispa.de>
2025-01-02 23:44:08 +0100

    Even nicer syntax error messages

--- src/fandango/language/parse.py
+++ src/fandango/language/parse.py
@@ -35,7 +35,7 @@ class MyErrorListener(ErrorListener):
         self.filename = filename
         super().__init__()
     def syntaxError(self, recognizer, offendingSymbol, line, column, msg, e):
-        raise SyntaxError(f'"{self.filename}", line {line}, column {column}: {msg}')
+        raise SyntaxError(f'{repr(self.filename)}, line {line}, column {column}: {msg}')
 
 
 def check_grammar_consistency(grammar, ignored_symbols=None, 
