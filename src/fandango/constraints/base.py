ec0720adc2726daeb2c55410fb50ce856988726c
Andreas Zeller <zeller@cispa.de>
2025-01-11 16:13:26 +0100

    Fix: don't exit if constraint evaluation fails

--- src/fandango/constraints/base.py
+++ src/fandango/constraints/base.py
@@ -199,7 +199,7 @@ class ComparisonConstraint(Constraint):
             except Exception as e:
                 e.add_note("Evaluation failed: " + self.left)
                 print_exception(e)
-                sys.exit("Evaluation failed: " + self.left)
+                continue
 
             try:
                 right = eval(self.right, self.global_variables, local_variables)
