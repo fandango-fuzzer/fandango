2417646b3e9b4a54bc547c1d26ebb90113ab0543
alex9849 <alexander@liggesmeyer.net>
2025-01-16 13:20:33 +0100

    assignment of a default role to automatically selected nonterminals

--- src/evaluation/experiments/simple_io/simple_io.py
+++ src/evaluation/experiments/simple_io/simple_io.py
@@ -1,13 +1,25 @@
 from fandango.evolution.algorithm import Fandango
-from fandango.language.grammar import FuzzingMode
+from fandango.language.grammar import *
+from fandango.language.symbol import *
 from fandango.language.parse import parse
 
 
 def main():
     # Parse grammar and constraints
-    with open("simple_io.fan") as f:
+    with open("simple_io_2.fan") as f:
         grammar, constraints = parse(f, use_stdlib=False)
     # Initialize FANDANGO with desired parameters
+    seen_non_terminals = set()
+    seen_non_terminals.add(NonTerminal('<start>'))
+    processed_non_terminals = set()
+    unprocessed_non_terminals = seen_non_terminals.difference(processed_non_terminals)
+    while len(unprocessed_non_terminals) > 0:
+        current_symbol = unprocessed_non_terminals.pop()
+        assigner = RoleAssigner('TEST', grammar)
+        assigner.visit(grammar.rules[current_symbol])
+        processed_non_terminals.add(current_symbol)
+        seen_non_terminals = seen_non_terminals.union(assigner.seen_non_terminals)
+        unprocessed_non_terminals = seen_non_terminals.difference(processed_non_terminals)
     fandango = Fandango(grammar=grammar, constraints=constraints, population_size=100)
 
     # Evolve solutions
