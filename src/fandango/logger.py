17ae2c4d4549d2be9ea58207db0b7cf3d6747275
Martin Eberlein <33397273+martineberlein@users.noreply.github.com>
2025-02-03 17:33:19 +0100

    disable logging for constraints when checking trees

--- src/fandango/logger.py
+++ src/fandango/logger.py
@@ -14,8 +14,8 @@ logging.basicConfig(
 
 def print_exception(e: Exception):
     LOGGER.info(traceback.format_exc().rstrip())
-    if not LOGGER.isEnabledFor(logging.INFO):
-        print(type(e).__name__ + ":", e, file=sys.stderr)
+    # if not LOGGER.isEnabledFor(logging.INFO):
+    #     print(type(e).__name__ + ":", e, file=sys.stderr)
     if "DerivationTree" in str(e):
         print(
             "Convert <symbol> to the expected type, say 'str(<symbol>)', 'int(<symbol>)', or 'float(<symbol>)'",
