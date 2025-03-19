e50dc013daac2db6d077eb1b607c0d06a7084d9e
Tim Scheckenbach <tisc00002@stud.uni-saarland.de>
2025-03-19 10:15:03 +0100

    fix and tests for #372 and #375

--- tests/test_grammar.py
+++ tests/test_grammar.py
@@ -92,3 +92,16 @@ class ConstraintTest(unittest.TestCase):
         solutions = self.get_solutions(GRAMMAR, c)
         for solution in solutions:
             self.assertNotEqual(solution, "10")
+
+    def test_generator_chunk(self):
+        file = open("tests/resources/generator_chunk.fan", "r")
+        GRAMMAR, c = parse(file, use_stdlib=True, use_cache=False)
+        fandango = Fandango(
+            grammar=GRAMMAR, constraints=c, desired_solutions=10, random_seed=6942012
+        )
+        solutions = fandango.evolve()
+        for solution in solutions:
+            sol = str(solution).split(".")
+            if len(sol[1]) > 2:
+                return
+        self.assertTrue(False)
