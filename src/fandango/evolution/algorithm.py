187e29d2885b7590059b791c5af92372c59d8c18
Andreas Zeller <zeller@cispa.de>
2025-03-31 11:32:57 +0200

    Fix for `TypeError: DerivationTree.replace() missing 1 required positional argument: 'new_subtree'`

--- src/fandango/evolution/algorithm.py
+++ src/fandango/evolution/algorithm.py
@@ -382,7 +382,7 @@ class Fandango:
                     )
                     if suggested_tree is None:
                         continue
-                    individual = individual.replace(failing_tree.tree, suggested_tree)
+                    individual = individual.replace(self.grammar, failing_tree.tree, suggested_tree)
                     self.fixes_made += 1
         return individual
 
