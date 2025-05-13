7022d41d4b3db335e827475a153e763ffe8538fa
alex9849 <alexander@liggesmeyer.net>
2025-05-13 15:33:43 +0200

    ftp

--- evaluation/experiments/ftp/ftp_parse_test.py
+++ evaluation/experiments/ftp/ftp_parse_test.py
@@ -5,7 +5,7 @@ from fandango.evolution.algorithm import Fandango, LoggerLevel
 from fandango.language.parse import parse
 from fandango.language.symbol import NonTerminal
 
-word = 'USER the_user\r\n'
+word = 'USER the_user'
 
 with open("ftp.fan") as f:
     grammar, constraints = parse(f, use_stdlib=False)
