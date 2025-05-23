8a9b0ba95897b0a8279344e9ff74980125a49b09
alex9849 <alexander@liggesmeyer.net>
2025-05-23 14:35:32 +0200

    ftp

--- evaluation/experiments/ftp/ftp_manual.py
+++ evaluation/experiments/ftp/ftp_manual.py
@@ -2,16 +2,6 @@ import random
 import socket
 import string
 
-
-def fuzz():
-    server_domain = 'localhost'
-    server_port = 21
-    sock = socket.socket(socket.AF_INET6, socket.SOCK_STREAM)
-    sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-    sock.connect((server_domain, server_port))
-    scan_socket(sock)
-    auth_failing(sock)
-
 def send_socket(sock, message):
     sock.sendall(message.encode("utf-8"))
 
@@ -21,7 +11,16 @@ def scan_socket(sock):
 def random_string():
     return ''.join(random.choice(string.ascii_letters + string.digits) for _ in range(random.randint(10, 20)))
 
+def fuzz():
+    server_domain = 'localhost'
+    server_port = 21
+    sock = socket.socket(socket.AF_INET6, socket.SOCK_STREAM)
+    sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+    sock.connect((server_domain, server_port))
+    auth_failing(sock)
+
 def auth_failing(sock):
+    scan_socket(sock)
     user_request = valid_login_user_msg()
     user_valid = True
     if random.random() < 0.5:
@@ -36,15 +35,20 @@ def auth_failing(sock):
     send_socket(sock, password_request)
     password_response = scan_socket(sock)
     assert password_response.startswith("530 ") and password_response.endswith("\r\n")
+    send_socket(sock, "QUIT\r\n")
+
 def valid_login_user_msg():
     return "USER correct_user\r\n"
+
 def invalid_login_user_msg():
     user_name = random_string()
     while user_name == "correct_user":
         user_name = random_string()
     return "USER " + user_name + "\r\n"
+
 def valid_login_password_msg():
     return "PASS correct_pass\r\n"
+
 def invalid_login_password_msg():
     password = random_string()
     while password == "correct_pass":
