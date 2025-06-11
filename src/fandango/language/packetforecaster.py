21a9614cd7b7e49eb4f37670ac1cf24d0aa40358
Valentin Huber <git@valentinhuber.me>
2025-06-11 15:44:18 +0200

    Remove unnecessary change

--- src/fandango/language/packetforecaster.py
+++ src/fandango/language/packetforecaster.py
@@ -393,6 +393,7 @@ class PacketForecaster:
             return repl_node
 
     class Parser(Grammar.Parser):
+
         def __init__(self, grammar: Grammar):
             super().__init__(grammar)
             self.reference_tree = None
