9e72bae74f33cd50f198959ff6a2f9bcd871efd6
Andreas Zeller <zeller@cispa.de>
2025-06-17 16:28:24 +0200

    Fix: "convert" no longer takes file options

--- tests/test_fan_parsers.py
+++ tests/test_fan_parsers.py
@@ -26,14 +26,14 @@ def test_file(fan_file):
     """Test the C++ and python .fan parsers for `fan_file`."""
 
     command = shlex.split(
-        f"fandango --parser=python convert --no-cache --no-stdlib {fan_file}"
+        f"fandango --parser=python convert {fan_file}"
     )
     python_out, err, return_code = run_command(command)
     assert return_code == 0
     assert err == ""
 
     command = shlex.split(
-        f"fandango --parser=cpp convert --no-cache --no-stdlib {fan_file}"
+        f"fandango --parser=cpp convert {fan_file}"
     )
     speedy_out, err, return_code = run_command(command)
     assert return_code == 0
