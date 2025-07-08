f72c0405250ef3905e1004e4d157b1b8338170fd
Valentin Huber <git@valentinhuber.me>
2025-07-08 16:02:46 +0200

    check if the output of the convert commands parse

--- tests/test_convert.py
+++ tests/test_convert.py
@@ -2,15 +2,22 @@
 
 import unittest
 
+from fandango.language.parse import parse
+
 from .utils import DOCS_ROOT, PROJECT_ROOT, run_command
 
 
 class test_convert(unittest.TestCase):
+    def _check_output(self, grammar: str, start_symbol: str) -> None:
+        parsed_grammar, _constraints = parse(grammar, start_symbol=start_symbol)
+        self.assertIsNotNone(parsed_grammar, grammar)
+
     def test_convert_fan(self):
         command = ["fandango", "convert", str(DOCS_ROOT / "persons.fan")]
         out, err, code = run_command(command)
         self.assertEqual(0, code, f"Command failed with code {code}: {err}")
         self.assertEqual(err, "")
+        self._check_output(out, "<start>")
 
     def test_convert_antlr(self):
         calculator = (
@@ -24,6 +31,7 @@ class test_convert(unittest.TestCase):
         out, err, code = run_command(command)
         self.assertEqual(0, code)
         self.assertEqual(err, "")
+        self._check_output(out, "<expression>")
 
     def test_convert_dtd(self):
         svg = (
@@ -42,6 +50,7 @@ class test_convert(unittest.TestCase):
         out, err, code = run_command(command)
         self.assertEqual(0, code, f"Command failed with code {code}: {err}")
         self.assertEqual(err, "")
+        self._check_output(out, "<svg>")
 
     def test_convert_bt(self):
         gif = PROJECT_ROOT / "src" / "fandango" / "converters" / "bt" / "gif.bt"
@@ -55,6 +64,7 @@ class test_convert(unittest.TestCase):
         out, err, code = run_command(command)
         self.assertEqual(0, code)
         self.assertEqual(err, "")
+        self._check_output(out, "<start>")
 
     def test_convert_bt_again(self):
         gif = PROJECT_ROOT / "src" / "fandango" / "converters" / "bt" / "gif.bt"
@@ -68,3 +78,4 @@ class test_convert(unittest.TestCase):
         out, err, code = run_command(command)
         self.assertEqual(0, code)
         self.assertEqual(err, "")
+        self._check_output(out, "<start>")
