65d3eaffec459207c18e7a57a8298975dabd45a5
Valentin Huber <git@valentinhuber.me>
2025-07-29 14:47:27 +0200

    Install bsdtar on Linux, ignore test on Windows

--- tests/test_evaluation.py
+++ tests/test_evaluation.py
@@ -1,8 +1,13 @@
 import subprocess
 import sys
 
+import pytest
+
 
 def test_run_evaluation_success():
+    if sys.platform.startswith("win"):
+        pytest.skip("bsdtar not supported on Windows")
+
     """Test that running `python -m evaluation.vs_isla.run_evaluation 1` exits with code 0."""
     result = subprocess.run(
         [sys.executable, "-m", "evaluation.vs_isla.run_evaluation", "1"],
