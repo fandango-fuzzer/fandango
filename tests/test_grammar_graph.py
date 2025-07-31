b5c4d1fee174409f616eeb0094c5297c2bf909f9
alex9849 <alexander@liggesmeyer.net>
2025-07-31 11:47:01 +0200

    Walk graph from existing tree

--- tests/test_grammar_graph.py
+++ tests/test_grammar_graph.py
@@ -10,7 +10,7 @@ from tests.utils import RESOURCES_ROOT, DOCS_ROOT
 def get_grammar(path):
     with open(path) as f:
         spec = f.read()
-    fandango = Fandango(spec, use_stdlib=False, use_cache=False)
+    fandango = Fandango(spec, use_stdlib=True, use_cache=False)
     return fandango.grammar
 
 
@@ -41,12 +41,12 @@ def test_graph_2():
     print(path)
 
 def test_graph_3():
-    grammar = get_grammar(DOCS_ROOT / "smtp_extended.fan")
+    grammar = get_grammar(DOCS_ROOT / "smtp-extended.fan")
     converter = GrammarGraphConverterVisitor(grammar.rules, NonTerminal("<start>"))
     graph = converter.process()
-    tree_to_continue = grammar.parse("ping\npong\n", mode=ParsingMode.INCOMPLETE, include_controlflow=True)
+    tree_to_continue = grammar.parse("220 abc ESMTP Postfix\r\nHELO abc\r\n", mode=ParsingMode.INCOMPLETE, include_controlflow=True)
     navigator = PacketNavigator(graph)
-    path = navigator.astar_tree(tree_to_continue, NonTerminal("<paff>"))
+    path = navigator.astar_tree(tree_to_continue, NonTerminal("<end_data>"))
     path = filter(lambda n: isinstance(n.node, NonTerminalNode) and n.node.sender is not None, path)
     path = map(lambda n: n.node.symbol, path)
     path = list(path)
