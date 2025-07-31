252ad57c15c0f802a743c8bee7f956986945e061
alex9849 <alexander@liggesmeyer.net>
2025-07-31 09:49:05 +0200

    Walk graph from existing tree

--- src/fandango/io/PacketNavigator.py
+++ src/fandango/io/PacketNavigator.py
@@ -1,6 +1,9 @@
 from astar import AStar
 
-from fandango.language.grammar.node_visitors.grammar_graph_converter import GrammarGraphNode, GrammarGraph
+from fandango.language import DerivationTree, Symbol, NonTerminal, Terminal
+from fandango.language.grammar.grammar_settings import GrammarSetting
+from fandango.language.grammar.node_visitors.grammar_graph_converter import GrammarGraphNode, GrammarGraph, \
+    LazyGrammarGraphNode, EagerGrammarGraphNode
 from fandango.language.grammar.nodes.non_terminal import NonTerminalNode
 from fandango.language.grammar.nodes.terminal import TerminalNode
 
@@ -26,3 +29,13 @@ class PacketNavigator(AStar[GrammarGraphNode]):
         if isinstance(current.node, NonTerminalNode) and isinstance(goal.node, NonTerminalNode):
             return current.node.symbol == goal.node.symbol
         return False
+
+    def astar_tree(self, tree: DerivationTree, symbol: Symbol):
+        start_node = self.graph.walk(tree)
+        if isinstance(symbol, NonTerminal):
+            symbol_node = NonTerminalNode(symbol, [])
+        elif isinstance(symbol, Terminal):
+            symbol_node = TerminalNode(symbol, [])
+        else:
+            raise ValueError(f"Unsupported symbol type: {type(symbol)}")
+        return self.astar(start_node, EagerGrammarGraphNode(symbol_node, []))
--- tests/test_grammar_graph.py
+++ tests/test_grammar_graph.py
@@ -1,6 +1,7 @@
 from fandango.api import Fandango
 from fandango.io.PacketNavigator import PacketNavigator
 from fandango.language import NonTerminal
+from fandango.language.grammar import ParsingMode
 from fandango.language.grammar.node_visitors.grammar_graph_converter import GrammarGraphConverterVisitor
 from fandango.language.grammar.nodes.non_terminal import NonTerminalNode
 from tests.utils import RESOURCES_ROOT
@@ -13,7 +14,7 @@ def get_grammar(path):
     return fandango.grammar
 
 
-def test_forecast_1():
+def test_graph_1():
     grammar = get_grammar(RESOURCES_ROOT / "minimal_io.fan")
     converter = GrammarGraphConverterVisitor(grammar.rules, NonTerminal("<start>"))
     graph = converter.process()
@@ -21,6 +22,19 @@ def test_forecast_1():
     start_node = graph.start.reaches[0].reaches[0].reaches[0]
     goal_node = graph.start.reaches[0].reaches[0].reaches[0].reaches[0].reaches[0].reaches[0].reaches[0]
     path = navigator.astar(start_node, goal_node)
+    path = filter(lambda n: isinstance(n.node, NonTerminalNode) and n.node.sender is not None, path)
+    path = map(lambda n: n.node.symbol, path)
+    path = list(path)
+    print(path)
+
+
+def test_graph_2():
+    grammar = get_grammar(RESOURCES_ROOT / "minimal_io.fan")
+    converter = GrammarGraphConverterVisitor(grammar.rules, NonTerminal("<start>"))
+    graph = converter.process()
+    tree_to_continue = grammar.parse("ping\npong\n", mode=ParsingMode.INCOMPLETE, include_controlflow=True)
+    navigator = PacketNavigator(graph)
+    path = navigator.astar_tree(tree_to_continue, NonTerminal("<paff>"))
     path = filter(lambda n: isinstance(n.node, NonTerminalNode) and n.node.sender is not None, path)
     path = map(lambda n: n.node.symbol, path)
     path = list(path)
