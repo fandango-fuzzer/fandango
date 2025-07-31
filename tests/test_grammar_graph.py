7338c55a929acbb49da7da39432b4725de5c602c
alex9849 <alexander@liggesmeyer.net>
2025-07-31 12:43:21 +0200

    Walk graph from existing tree

--- tests/test_grammar_graph.py
+++ tests/test_grammar_graph.py
@@ -1,9 +1,10 @@
 from fandango.api import Fandango
-from fandango.io.PacketNavigator import PacketNavigator
+from fandango.io.PacketNavigator import GrammarNavigator
 from fandango.language import NonTerminal
 from fandango.language.grammar import ParsingMode
 from fandango.language.grammar.node_visitors.grammar_graph_converter import GrammarGraphConverterVisitor
 from fandango.language.grammar.nodes.non_terminal import NonTerminalNode
+from fandango.language.grammar.nodes.terminal import TerminalNode
 from tests.utils import RESOURCES_ROOT, DOCS_ROOT
 
 
@@ -18,7 +19,7 @@ def test_graph_1():
     grammar = get_grammar(RESOURCES_ROOT / "minimal_io.fan")
     converter = GrammarGraphConverterVisitor(grammar.rules, NonTerminal("<start>"))
     graph = converter.process()
-    navigator = PacketNavigator(graph)
+    navigator = GrammarNavigator(graph)
     start_node = graph.start.reaches[0].reaches[0].reaches[0]
     goal_node = graph.start.reaches[0].reaches[0].reaches[0].reaches[0].reaches[0].reaches[0].reaches[0]
     path = navigator.astar(start_node, goal_node)
@@ -33,7 +34,7 @@ def test_graph_2():
     converter = GrammarGraphConverterVisitor(grammar.rules, NonTerminal("<start>"))
     graph = converter.process()
     tree_to_continue = grammar.parse("ping\npong\n", mode=ParsingMode.INCOMPLETE, include_controlflow=True)
-    navigator = PacketNavigator(graph)
+    navigator = GrammarNavigator(graph)
     path = navigator.astar_tree(tree_to_continue, NonTerminal("<paff>"))
     path = filter(lambda n: isinstance(n.node, NonTerminalNode) and n.node.sender is not None, path)
     path = map(lambda n: n.node.symbol, path)
@@ -45,9 +46,10 @@ def test_graph_3():
     converter = GrammarGraphConverterVisitor(grammar.rules, NonTerminal("<start>"))
     graph = converter.process()
     tree_to_continue = grammar.parse("220 abc ESMTP Postfix\r\nHELO abc\r\n", mode=ParsingMode.INCOMPLETE, include_controlflow=True)
-    navigator = PacketNavigator(graph)
+    navigator = GrammarNavigator(graph)
+    navigator.set_message_cost(1)
     path = navigator.astar_tree(tree_to_continue, NonTerminal("<end_data>"))
-    path = filter(lambda n: isinstance(n.node, NonTerminalNode) and n.node.sender is not None, path)
+    path = filter(lambda n: isinstance(n.node, (NonTerminalNode, TerminalNode)), path)
     path = map(lambda n: n.node.symbol, path)
     path = list(path)
     print(path)
