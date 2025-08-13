154d9f4d59be5cc6961524fc001e702a19e5fae7
alex9849 <alexander@liggesmeyer.net>
2025-08-13 12:10:56 +0200

    logging

--- src/fandango/io/packetparser.py
+++ src/fandango/io/packetparser.py
@@ -98,11 +98,13 @@ def parse_next_remote_packet(
             )
             if time.time() - start_time > wait_for_completion_time:
                 if len(complete_parses) == 0:
+                    incomplete_nt_list = map(lambda x: repr(x), available_non_terminals)
+                    nt_list = map(lambda x: repr(x), forecast_non_terminals.get_non_terminals())
                     raise FandangoFailedError(
-                        "Timeout while waiting for next message fragment from "
-                        + msg_sender
-                        + ". Messages: "
-                        + str(io_instance.get_received_msgs())
+                        f"Timeout while waiting for next message fragment from {msg_sender}. "
+                        + f"Remaining NonTerminals: {str(" | ".join(incomplete_nt_list))} "
+                        + f"Applicable NonTerminals: {str(" | ".join(nt_list))} "
+                        + f"Received messages: {str(io_instance.get_received_msgs())}"
                     )
                 else:
                     continue_parse = False
