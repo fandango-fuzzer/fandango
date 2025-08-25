4ee30f74cce097779b5fc474974f398973a5492b
alex9849 <alexander@liggesmeyer.net>
2025-08-25 14:15:31 +0200

    Added powerschedule guidiance

--- src/fandango/io/navigation/powerschedule.py
+++ src/fandango/io/navigation/powerschedule.py
@@ -1,7 +1,9 @@
+import random
 from collections import Counter
 from typing import Sequence
 
 from fandango.io.navigation.PacketNonTerminal import PacketNonTerminal
+from fandango.language.symbols.non_terminal import NonTerminal
 
 
 class PowerSchedule:
@@ -10,20 +12,22 @@ class PowerSchedule:
         self.energy = dict()
         self.exponent = 2.0
 
-    def assign_energy(self, packet_types: Sequence[PacketNonTerminal], coverage: dict[PacketNonTerminal, float]):
+    def assign_energy(self, packet_types: Sequence[NonTerminal], coverage: dict[NonTerminal, float]):
         frequencies = Counter(packet_types)
         self.energy = dict()
 
-
-        alpha = 0.9
-
-        for type in packet_types:
-            self.energy[type] = (1 - distance(type)) * (1 - t_exp) + 0.5 * t_exp
-
         for p_type, freq in frequencies.items():
-            self.energy[p_type] = 1 / (freq**self.exponent)
+            if p_type in coverage:
+                coverage_val = coverage[p_type]
+            else:
+                coverage_val = 0.0
+            self.energy[p_type] = 1 / (freq**self.exponent) * (1 - coverage_val)
+        for p_type in coverage.keys():
+            if p_type not in self.energy:
+                self.energy[p_type] = 1 - coverage[p_type]
+        self.energy = self._normalize_energy()
 
-    def normalized_energy(self) -> dict[PacketNonTerminal, float]:
+    def _normalize_energy(self) -> dict[PacketNonTerminal, float]:
         sum_energy = sum(self.energy.values())
         assert sum_energy != 0
         norm_energy = dict(
@@ -31,5 +35,8 @@ class PowerSchedule:
         )
         return norm_energy
 
-    def choose(self):
-        pass
+    def choose(self) -> NonTerminal:
+        energy_list = list(self.energy.items())
+        key_list = list(map(lambda item: item[0], energy_list))
+        value_list = list(map(lambda item: item[1], energy_list))
+        return random.choices(key_list, weights=value_list, k=1)[0]
