e6eb7f517650a44782b73a5f71f7c68034f445e8
alex9849 <alexander@liggesmeyer.net>
2025-08-30 13:49:29 +0200

    cleanup

---
+++ src/fandango/io/navigation/pathutils.py
@@ -0,0 +1,9 @@
+
+def find_longest_suffix(path, suffix_path):
+    max_overlap = 0
+    search_len = len(suffix_path)
+    chain_len = len(path)
+    for i in range(1, min(search_len, chain_len) + 1):
+        if path[-i:] == suffix_path[:i]:
+            max_overlap = i
+    return suffix_path[:max_overlap]
