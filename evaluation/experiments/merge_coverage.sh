bec048f6f54cccebe183a0745ae41f3613ef97dd
alex9849 <alexander@liggesmeyer.net>
2025-09-11 13:55:22 +0200

    more detailed coverage metrics

--- evaluation/experiments/merge_coverage.sh
+++ evaluation/experiments/merge_coverage.sh
@@ -1,16 +1,32 @@
 #!/bin/bash
 
-python3 multi_run_merger.py ./smtp/coverage_w_guidance ./smtp_median_message_coverage_guided.csv "percent___role_unique_all_party,percent___role_unique_Server,percent___role_unique_Client"
-python3 multi_run_merger.py ./smtp/coverage_wo_guidance ./smtp_median_message_coverage_unguided.csv "percent___role_unique_all_party,percent___role_unique_Server,percent___role_unique_Client"
+python3 multi_run_merger.py ./smtp/coverage_w_guidance ./coverage_merged/msgs/smtp_median_grammar_coverage_guided.csv "percent___role_unique_Client"
+python3 multi_run_merger.py ./smtp/coverage_wo_guidance ./coverage_merged/msgs/smtp_median_grammar_coverage_unguided.csv "percent___role_unique_Client"
 
-python3 multi_run_merger.py ./dns/coverage_w_guidance ./dns_median_message_coverage_guided.csv "percent___role_unique_all_party,percent___role_unique_Server,percent___role_unique_Client"
-python3 multi_run_merger.py ./dns/coverage_wo_guidance ./dns_median_message_coverage_unguided.csv "percent___role_unique_all_party,percent___role_unique_Server,percent___role_unique_Client"
+python3 multi_run_merger.py ./dns/coverage_w_guidance ./coverage_merged/msgs/dns_median_grammar_coverage_guided.csv "percent___role_unique_Client"
+python3 multi_run_merger.py ./dns/coverage_wo_guidance ./coverage_merged/msgs/dns_median_grammar_coverage_unguided.csv "percent___role_unique_Client"
 
-python3 multi_run_merger.py ./ftp/coverage_w_guidance ./ftp_median_message_coverage_guided.csv "percent___role_unique_all_party,percent___role_unique_ServerControl,percent___role_unique_ClientControl,percent___role_unique_ServerData"
-python3 multi_run_merger.py ./ftp/coverage_wo_guidance ./ftp_median_message_coverage_unguided.csv "percent___role_unique_all_party,percent___role_unique_ServerControl,percent___role_unique_ClientControl,percent___role_unique_ServerData"
+python3 multi_run_merger.py ./ftp/coverage_w_guidance ./coverage_merged/msgs/ftp_median_grammar_coverage_guided.csv "percent___role_unique_ClientControl"
+python3 multi_run_merger.py ./ftp/coverage_wo_guidance ./coverage_merged/msgs/ftp_median_grammar_coverage_unguided.csv "percent___role_unique_ClientControl"
 
-python3 multi_run_merger.py ./dune/coverage_w_guidance ./dune_median_message_coverage_guided.csv "percent___role_unique_all_party,percent___role_unique_Server,percent___role_unique_Client"
-python3 multi_run_merger.py ./dune/coverage_wo_guidance ./dune_median_message_coverage_unguided.csv "percent___role_unique_all_party,percent___role_unique_Server,percent___role_unique_Client"
+python3 multi_run_merger.py ./dune/coverage_w_guidance ./coverage_merged/msgs/dune_median_grammar_coverage_guided.csv "percent___role_unique_Client"
+python3 multi_run_merger.py ./dune/coverage_wo_guidance ./coverage_merged/msgs/dune_median_grammar_coverage_unguided.csv "percent___role_unique_Client"
 
-python3 multi_run_merger.py ./chatgpt/coverage_w_guidance ./chatgpt_median_message_coverage_guided.csv "percent___role_unique_all_party,percent___role_unique_Gpt,percent___role_unique_Client"
-python3 multi_run_merger.py ./chatgpt/coverage_wo_guidance ./chatgpt_median_message_coverage_unguided.csv "percent___role_unique_all_party,percent___role_unique_Gpt,percent___role_unique_Client"
+python3 multi_run_merger.py ./chatgpt/coverage_w_guidance ./coverage_merged/msgs/chatgpt_median_grammar_coverage_guided.csv "percent___role_unique_Client"
+python3 multi_run_merger.py ./chatgpt/coverage_wo_guidance ./coverage_merged/msgs/chatgpt_median_grammar_coverage_unguided.csv "percent___role_unique_Client"
+
+
+python3 multi_run_merger.py ./smtp/coverage_w_guidance ./coverage_merged/overall/smtp_median_grammar_coverage_guided.csv "percent_<start>"
+python3 multi_run_merger.py ./smtp/coverage_wo_guidance ./coverage_merged/overall/smtp_median_grammar_coverage_unguided.csv "percent_<start>"
+
+python3 multi_run_merger.py ./dns/coverage_w_guidance ./coverage_merged/overall/dns_median_grammar_coverage_guided.csv "percent_<start>"
+python3 multi_run_merger.py ./dns/coverage_wo_guidance ./coverage_merged/overall/dns_median_grammar_coverage_unguided.csv "percent_<start>"
+
+python3 multi_run_merger.py ./ftp/coverage_w_guidance ./coverage_merged/overall/ftp_median_grammar_coverage_guided.csv "percent_<start>"
+python3 multi_run_merger.py ./ftp/coverage_wo_guidance ./coverage_merged/overall/ftp_median_grammar_coverage_unguided.csv "percent_<start>"
+
+python3 multi_run_merger.py ./dune/coverage_w_guidance ./coverage_merged/overall/dune_median_grammar_coverage_guided.csv "percent_<start>"
+python3 multi_run_merger.py ./dune/coverage_wo_guidance ./coverage_merged/overall/dune_median_grammar_coverage_unguided.csv "percent_<start>"
+
+python3 multi_run_merger.py ./chatgpt/coverage_w_guidance ./coverage_merged/overall/chatgpt_median_grammar_coverage_guided.csv "percent_<start>"
+python3 multi_run_merger.py ./chatgpt/coverage_wo_guidance ./coverage_merged/overall/chatgpt_median_grammar_coverage_unguided.csv "percent_<start>"
