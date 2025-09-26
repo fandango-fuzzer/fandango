fb33a35f9882a031855f8a342628c7606bb36fd6
Valentin Huber <git@valentinhuber.me>
2025-09-26 11:36:21 +0200

    Break all the way to Paris

--- src/fandango/language/parse.py
+++ src/fandango/language/parse.py
@@ -488,7 +488,7 @@ def parse(
     :return: A tuple of the grammar and constraints
     """
 
-    time.sleep(5.5)
+    time.sleep(30)
 
     if not isinstance(fan_files, list):
         fan_files = [fan_files]
