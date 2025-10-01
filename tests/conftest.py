995bb0be44e24dba237853ae6e472de0aaea997f
Valentin Huber <git@valentinhuber.me>
2025-10-01 17:21:46 +0200

    Run benchmarks first in unit tests to better use parallelization

--- tests/conftest.py
+++ tests/conftest.py
@@ -18,7 +18,14 @@ def pytest_configure(config: pytest.Config):
 def pytest_collection_modifyitems(items: list[pytest.Item]):
     # ensure long-running tests are run first to balance loading across cores
     # so we have to wait less for a single long test to finish running because it was scheduled last
-    order = ["evaluation", "cli", "softconstraint", "optimizer", "fan_parsers"]
+    order = [
+        "benchmark",
+        "evaluation",
+        "cli",
+        "softconstraint",
+        "optimizer",
+        "fan_parsers",
+    ]
     priority = [f"tests/test_{test}.py" for test in order]
     items.sort(
         key=lambda x: (
