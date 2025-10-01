c018573e2f6775beba2cd38b4c7d06a97287d12f
Valentin Huber <git@valentinhuber.me>
2025-10-01 12:59:01 +0200

    beartype fixes

--- src/fandango/beartype.py
+++ src/fandango/beartype.py
@@ -3,6 +3,9 @@ def activate_beartype() -> None:
     from beartype import BeartypeConf
 
     skip_packages = (
+        "fandango.language.grammar.grammar",  # GrammarProcessor sometimes passes searches instead of NonTerminalNodes to Grammar.set_generator
+        "fandango.language.grammar.literal_generator",  # The above is then passed to the constructor of LiteralGenerator
+        "fandango.converters.dtd.DTDFandangoConverter",  # the type seems wrong
         "fandango.converters.antlr.ANTLRv4Parser",  # auto-generated
         "fandango.converters.antlr.ANTLRv4Lexer",  # auto-generated
         "fandango.language.parser.FandangoLexer",  # auto-generated
