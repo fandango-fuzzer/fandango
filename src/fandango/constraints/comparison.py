a56581e508a98b0567586c0c4d736c7f76b8c56a
alex9849 <alexander@liggesmeyer.net>
2026-01-13 14:09:35 +0100

    fixed black

--- src/fandango/constraints/comparison.py
+++ src/fandango/constraints/comparison.py
@@ -39,7 +39,9 @@ class EqualComparisonSuggestion(Suggestion):
         self._target = target
         self._source = source
 
-    def rec_set_allow_repetition_full_delete(self, allow_repetition_full_delete: bool) -> None:
+    def rec_set_allow_repetition_full_delete(
+        self, allow_repetition_full_delete: bool
+    ) -> None:
         pass
 
     def get_replacements(
