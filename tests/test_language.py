c3a7fa1dff3a4af9f636a76f78ceaf63225147e9
Andreas Zeller <zeller@cispa.de>
2026-01-19 15:13:16 +0100

    Black

--- tests/test_language.py
+++ tests/test_language.py
@@ -43,11 +43,13 @@ def get_tree(example, start="fandango"):
 
 
 def test_indents():
-    tree = get_tree(r"""
+    tree = get_tree(
+        r"""
 <a> ::= \
     "a" \
     | "a" <a>
-""")
+"""
+    )
     splitter = FandangoSplitter()
     splitter.visit(tree)
     processor = GrammarProcessor(grammar_settings=splitter.grammar_settings)
@@ -59,12 +61,14 @@ def test_indents():
 
 
 def test_alt_indents():
-    tree = get_tree(r"""
+    tree = get_tree(
+        r"""
 <a> ::= (
     "a"
     | "a" <a>
     )
-""")
+"""
+    )
     splitter = FandangoSplitter()
     splitter.visit(tree)
     processor = GrammarProcessor(grammar_settings=splitter.grammar_settings)
