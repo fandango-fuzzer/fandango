408ebfcccc9ce730cb1d818beb9d236f84809ca6
Valentin Huber <git@valentinhuber.me>
2026-02-26 11:43:49 +0100

    Simplify dependency lookups

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,4 +1,7 @@
 import os
+from pathlib import Path
+from shutil import which
+
 from fandango.beartype import activate_beartype
 import pytest
 
@@ -13,6 +16,25 @@ def pytest_configure(config: pytest.Config):
         activate_beartype()
     else:
         print("Skipping beartype because FANDANGO_FORCE_SKIP_BEARTYPE is set")
+
+    if which("fan") is None or which("fcc") is None:
+        # Ensure we add an absolute path so it works from any cwd
+        repo_root = Path(__file__).resolve().parents[1]
+        tool_dir = repo_root / "fcc" / "llvm" / "build" / "compiler"
+        if not tool_dir.exists():
+            raise AssertionError("fcc compiler was not found; run `make test-tools`")
+        os.environ["PATH"] = os.environ["PATH"] + os.pathsep + str(tool_dir)
+
+    if which("llvm-config") is None:
+        homebrew_llvm_path = Path("/opt/homebrew/opt/llvm@18/bin")
+        if homebrew_llvm_path.exists():
+            os.environ["PATH"] = (
+                str(homebrew_llvm_path) + os.pathsep + os.environ["PATH"]
+            )
+        else:
+            raise AssertionError(
+                f"LLVM is required for tests; make sure to add the same version to the PATH as for building fcc"
+            )
 
 
 def pytest_collection_modifyitems(items: list[pytest.Item]):
