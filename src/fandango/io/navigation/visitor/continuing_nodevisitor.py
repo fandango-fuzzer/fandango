d224db1fa11c06c3aa85cc326bc9bec6232abe97
alex9849 <alexander@liggesmeyer.net>
2026-06-16 17:34:21 +0200

    fixed collapsed path in continuing_nodevisitor.py

--- src/fandango/io/navigation/visitor/continuing_nodevisitor.py
+++ src/fandango/io/navigation/visitor/continuing_nodevisitor.py
@@ -34,15 +34,18 @@ class ContinuingNodeVisitor(NodeVisitor[None, bool]):
         self.current_tree = [None]
         if self.tree is not None:
             self.current_path.append((self.tree.nonterminal, False))
+            self.current_path_collapsed.append((self.tree.nonterminal, False))
             if len(self.tree.children) != 0:
                 self.current_tree = [[self.tree.children[0]]]
             self.visit(self.grammar.rules[self.current_path[-1][0]])
         else:
             self.current_path.append((NonTerminal("<start>"), True))
+            self.current_path_collapsed.append((NonTerminal("<start>"), True))
             self.visit(NonTerminalNode(NonTerminal("<start>"), []))
 
         self.current_tree.pop()
         self.current_path.pop()
+        self.current_path_collapsed.pop()
 
     def on_enter_controlflow(self, expected_nt: str) -> None:
         tree = self.current_tree[-1]
@@ -138,8 +141,8 @@ class ContinuingNodeVisitor(NodeVisitor[None, bool]):
                 try:
                     continue_exploring = self.visit(alt)
                     found = True
-                    if not node.is_permutation:
-                        break
+                    #if not node.is_permutation:
+                    #    break
                 except GrammarKeyError:
                     self.current_tree = fallback_tree
                     self.current_path = fallback_path
