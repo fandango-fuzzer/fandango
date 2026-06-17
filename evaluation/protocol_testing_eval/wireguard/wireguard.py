5a1064406de146aefba91de840629d11bf1c206d
alex9849 <alexander@liggesmeyer.net>
2026-06-17 13:44:56 +0200

    added wireguard docker server config

---
+++ evaluation/protocol_testing_eval/wireguard/wireguard.py
@@ -0,0 +1,32 @@
+from fandango.evolution.algorithm import SimpleGeneticAlgorithm, LoggerLevel, ProtocolAlgorithm
+from fandango.io.navigation.coverage_goal import CoverageGoal
+from fandango.language.grammar import FuzzingMode
+from fandango.language.parse.parse import parse
+
+
+def main():
+    # Parse grammar and constraints
+    with open("wireguard.fan") as f:
+        grammar, constraints = parse(
+            f,
+            use_stdlib=False,
+        )
+    assert grammar is not None
+
+    fandango = SimpleGeneticAlgorithm(
+        grammar=grammar,
+        constraints=constraints,
+        logger_level=LoggerLevel.INFO,
+        coverage_goal=CoverageGoal.STATE_INPUTS_OUTPUTS,
+    )
+    fandango = ProtocolAlgorithm(
+        packet_algorithm=fandango,
+        coverage_goal=CoverageGoal.STATE_INPUTS_OUTPUTS,
+    )
+
+    for solution in fandango.generate(mode=FuzzingMode.IO, max_generations=3):
+        pass
+
+
+if __name__ == "__main__":
+    main()
