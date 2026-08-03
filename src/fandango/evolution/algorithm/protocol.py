a80dff8c59c55772492cd730ec1bff39da56a248
alex9849 <alexander@liggesmeyer.net>
2026-08-03 14:27:14 +0200

    added bugfix from different branch

--- src/fandango/evolution/algorithm/protocol.py
+++ src/fandango/evolution/algorithm/protocol.py
@@ -61,7 +61,7 @@ class ProtocolAlgorithm(GeneticAlgorithm):
     def _wait_for_remote_message(self, timeout: int) -> bool:
         wait_start = time.time()
         while not self._io_instance.received_msg():
-            if time.time() - wait_start > timeout:
+            if time.time() - wait_start > timeout and timeout >= 0:
                 return False
             time.sleep(0.025)
         return True
