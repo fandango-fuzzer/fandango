f597944377568c912c6ea8a7e47ddbf227de52b8
alex9849 <alexander@liggesmeyer.net>
2026-09-07 09:01:29 +0200

    moved symbol hashing to superclass

--- src/fandango/language/symbols/non_terminal.py
+++ src/fandango/language/symbols/non_terminal.py
@@ -5,16 +5,12 @@ class NonTerminal(Symbol):
     def __init__(self, symbol: str) -> None:
         assert isinstance(symbol, str)
         super().__init__(symbol, SymbolType.NON_TERMINAL)
-        self._hash = hash((self._value, self._type))
 
     def name(self) -> str:
         """
         Return the name of the non-terminal symbol.
         """
         return str(self._value)
-
-    def __hash__(self) -> int:
-        return self._hash
 
     def format_as_spec(self) -> str:
         return self.name()
